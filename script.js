ce6abda044d47df7991a9cff0427734b37429682
ArthurRizk <fnbolt1@gmail.com>
2025-05-08 16:54:42 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Hover effect for template cards
+document.querySelectorAll('.template-card').forEach((card) => {
+  card.addEventListener('mouseenter', () => {
+    card.style.transform = 'scale(1.05)';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'scale(1)';
+  });
+});
